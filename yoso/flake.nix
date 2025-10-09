f3b9afd203b42ca494f6215efe1ee1ac6804c91b
medmekk <mohamed.mekkouri@huggingface.co>
2025-10-09 23:49:42 +0000

    add yoso

---
+++ yoso/flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Flake for yoso kernels";
+
+  inputs = {
+    kernel-builder.url = "github:huggingface/kernel-builder/";
+  };
+
+  outputs =
+    {
+      self,
+      kernel-builder,
+    }:
+    kernel-builder.lib.genFlakeOutputs {
+      path = ./.;
+      rev = self.shortRev or self.dirtyShortRev or self.lastModifiedDate;
+    };
+}
